d973a001f7e2b621703070cd27905b4c37bdf450
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2023-02-10 23:51:56 -0500

    Add missing styled component for Admin page

---
+++ src/components/admin/Admin.styled.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const Header = styled.h3`
+    display: flex;
+    border: 1px solid;
+    align-items: center;
+    width: fit-content;
+`;
+
+Header.Item = styled.div`
+    min-width: ${props => props.lg ? '186' : '136'}px;
+    text-align: center;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+        cursor: pointer;
+        background-color: rgba(0, 0, 0, 0.1);;
+    }
+`;
+
+export const List = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
+export const Row = styled.div`
+    display: flex;
+`;
+
+Row.Item = styled.div`
+    min-width: ${props => props.lg ? '186' : '136'}px;
+    text-align: center;
+`;
